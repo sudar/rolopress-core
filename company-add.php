a8864d5e0ab79303e8efd1c139dc333577862d5b
sbruner <sbruner@slipfire.com>
2009-09-16 02:53:39 +0000

    fixed comment class moved templates back to main directory
    git-svn-id: http://svn.sudarmuthu.com/rolopress/rolopress-core/trunk@28 38a0677f-509c-40db-b594-c9751fff4674

---
+++ company-add.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Template Name: Company: Add
+ *
+ * add companies
+ *
+ * @package RoloPress
+ * @subpackage Template
+ */
+
+get_header(); ?>
+	
+		/* insert code */
+		
+<?php get_sidebar(); ?>	
+<?php get_footer(); ?>
