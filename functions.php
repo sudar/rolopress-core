062533a6e8b9ce45726c86be57321cb005700933
Sudar Muthu <sudar@sudarmuthu.com>
2009-12-29 06:08:16 +0000

    Added compat.php file containing helper functions
    git-svn-id: http://svn.sudarmuthu.com/rolopress/rolopress-core/trunk@131 38a0677f-509c-40db-b594-c9751fff4674

--- functions.php
+++ functions.php
@@ -9,54 +9,58 @@
  * @subpackage Functions
  */
 
-
 // Define constant paths
-	define( ROLOPRESS_DIR, TEMPLATEPATH );
-	define( ROLOPRESS_LIBRARY, ROLOPRESS_DIR . '/library' );
-	define( ROLOPRESS_EXTENSIONS, ROLOPRESS_LIBRARY . '/extensions' );
-	define( ROLOPRESS_FUNCTIONS, ROLOPRESS_LIBRARY . '/functions' );
-	define( ROLOPRESS_SETUP, ROLOPRESS_LIBRARY . '/setup' );
-	define( ROLOPRESS_WIDGETS, ROLOPRESS_LIBRARY . '/widgets' );
-	define( ROLOPRESS_INCLUDES, ROLOPRESS_LIBRARY . '/includes' );
+define( ROLOPRESS_DIR, TEMPLATEPATH );
+define( ROLOPRESS_LIBRARY, ROLOPRESS_DIR . '/library' );
+define( ROLOPRESS_EXTENSIONS, ROLOPRESS_LIBRARY . '/extensions' );
+define( ROLOPRESS_FUNCTIONS, ROLOPRESS_LIBRARY . '/functions' );
+define( ROLOPRESS_SETUP, ROLOPRESS_LIBRARY . '/setup' );
+define( ROLOPRESS_WIDGETS, ROLOPRESS_LIBRARY . '/widgets' );
+define( ROLOPRESS_INCLUDES, ROLOPRESS_LIBRARY . '/includes' );
 
 // Define constant paths (other file types)
-	$rolopress_dir = get_bloginfo( 'template_directory' );
-	define( ROLOPRESS_IMAGES, $rolopress_dir . '/library/images' );
-	define( ROLOPRESS_CSS, $rolopress_dir . '/library/styles' );
-	define( ROLOPRESS_JS, $rolopress_dir . '/library/js' );
+$rolopress_dir = get_bloginfo( 'template_directory' );
+define( ROLOPRESS_IMAGES, $rolopress_dir . '/library/images' );
+define( ROLOPRESS_CSS, $rolopress_dir . '/library/styles' );
+define( ROLOPRESS_JS, $rolopress_dir . '/library/js' );
 
 // Define child theme paths
-	define( ROLOPRESS_CHILD_DIR, get_stylesheet_directory() );
-	define( ROLOPRESS_CHILD_URL, get_stylesheet_directory_uri() );
+define( ROLOPRESS_CHILD_DIR, get_stylesheet_directory() );
+define( ROLOPRESS_CHILD_URL, get_stylesheet_directory_uri() );
+
+// Load compatability function
+require_once( ROLOPRESS_FUNCTIONS . '/compat.php' );
 
 // Load action hooks
-	require_once( ROLOPRESS_FUNCTIONS . '/hooks-actions.php' );
-	
+require_once( ROLOPRESS_FUNCTIONS . '/hooks-actions.php' );
+
 // Run default setup
-	require_once( ROLOPRESS_SETUP . '/setup-fields.php' );
-	
-	if ( is_admin() && isset($_GET['activated'] ) && $pagenow == "themes.php" ) // Code based on http://www.nabble.com/Activation-hook-exist-for-themes--td25211004.html
-		require_once( ROLOPRESS_SETUP . '/add-pages.php' );
-	
+require_once( ROLOPRESS_SETUP . '/setup-fields.php' );
+
+// Code based on http://www.nabble.com/Activation-hook-exist-for-themes--td25211004.html
+if ( is_admin() && isset($_GET['activated'] ) && $pagenow == "themes.php" ) {
+    require_once( ROLOPRESS_SETUP . '/add-pages.php' );
+}
+
 // Load RoloPress functions
-	require_once( ROLOPRESS_FUNCTIONS . '/template-functions.php' );
-	require_once( ROLOPRESS_FUNCTIONS . '/contact-functions.php' );
-	require_once( ROLOPRESS_FUNCTIONS . '/company-functions.php' );
-	require_once( ROLOPRESS_FUNCTIONS . '/note-functions.php' );
-	require_once( ROLOPRESS_FUNCTIONS . '/dynamic-classes.php' );
-	
+require_once( ROLOPRESS_FUNCTIONS . '/template-functions.php' );
+require_once( ROLOPRESS_FUNCTIONS . '/contact-functions.php' );
+require_once( ROLOPRESS_FUNCTIONS . '/company-functions.php' );
+require_once( ROLOPRESS_FUNCTIONS . '/note-functions.php' );
+require_once( ROLOPRESS_FUNCTIONS . '/dynamic-classes.php' );
+
 // Load widget areas and custom widgets
-	require_once( ROLOPRESS_FUNCTIONS . '/widgets.php' );
-	
+require_once( ROLOPRESS_FUNCTIONS . '/widgets.php' );
+
 // Load extensions
-	require_once( ROLOPRESS_EXTENSIONS . '/rolosearch/rolosearch.php' );
-	require_once( ROLOPRESS_EXTENSIONS . '/query-multiple-taxonomies/query-multiple-taxonomies.php' );
-	
+require_once( ROLOPRESS_EXTENSIONS . '/rolosearch/rolosearch.php' );
+require_once( ROLOPRESS_EXTENSIONS . '/query-multiple-taxonomies/query-multiple-taxonomies.php' );
+
 // Load javascript - only if user has proper permissions
-	if ( current_user_can('edit_posts') ) { require_once( ROLOPRESS_INCLUDES . '/js-load.php' ); } 
-	
+if ( current_user_can('edit_posts') ) { require_once( ROLOPRESS_INCLUDES . '/js-load.php' ); }
+
 // Change admin section to RoloPress style
-	if (function_exists ('login_header') || is_admin()) // only load if viewing admin
-		require_once( ROLOPRESS_FUNCTIONS . '/admin.php' );
+if (function_exists ('login_header') || is_admin()) // only load if viewing admin
+    require_once( ROLOPRESS_FUNCTIONS . '/admin.php' );
 
 ?>
---
+++ library/functions/compat.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Rolopress implementation for PHP functions missing from older PHP versions and WordPress functions which *should* be in core
+ *
+ * @package PHP
+ * @access private
+ */
+
+/**
+ * Retrieve a post given its title.
+ *
+ * @uses $wpdb
+ *
+ * @param string $post_title Post title
+ * @param string $output Optional. Output type. OBJECT, ARRAY_N, or ARRAY_A.
+ * @return mixed Post data if successful (type depends on $output) | Null if no posts found
+ */
+if (!function_exists('get_post_by_title'))  {
+    // this will go away if this WordPress ticket is blessed <http://core.trac.wordpress.org/ticket/10865>
+
+    function get_post_by_title($post_title, $output = OBJECT) {
+        global $wpdb;
+        $post = $wpdb->get_var( $wpdb->prepare( "SELECT ID FROM $wpdb->posts WHERE post_title = %s AND post_type='post'", $post_title ));
+        if ( $post )
+            return get_post($post, $output);
+
+        return null;
+    }
+}
+
+?>
