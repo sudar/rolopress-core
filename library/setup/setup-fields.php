49015de190e3d9ccd6fc9df62386368da61d718b
Sudar Muthu <sudar@sudarmuthu.com>
2009-12-29 05:54:12 +0000

    Removed rolo_create_meta_box() function, which is no longer needed
    git-svn-id: http://svn.sudarmuthu.com/rolopress/rolopress-core/trunk@130 38a0677f-509c-40db-b594-c9751fff4674

--- library/setup/setup-fields.php
+++ library/setup/setup-fields.php
@@ -241,62 +241,6 @@ array
 );
 
 /**
- * @global <type> $post
- * @global <type> $contact_fields
- * @since 0.1
- */
-function rolo_append_meta_box() {
-    global $post, $contact_fields;
-
-    $rolo_tab_index = 1000;
-    foreach($contact_fields as $contact_field) {
-        $meta_box_value = get_post_meta($post->ID, $contact_field['name'].'_rolo_value', true);
-        if($meta_box_value == '')
-            $meta_box_value = $contact_field['std'];
-        echo'<input type="hidden" name="'.$contact_field['name'].'_noncename" id="'.$contact_field['name'].'_noncename" value="'.wp_create_nonce( plugin_basename(__FILE__) ).'" />';
-        echo'<p><label style="letter-spacing:1px; text-transform:uppercase; color:#777;" for="'.$contact_field['name'].'_rolo_value">'.$contact_field['title'].'</label><br/>';
-        echo'<input style="padding:4px; font-weight:bold; border-top:1px solid #ccc; border-right:1px solid #ddd; border-bottom:1px solid #ddd; border-left:1px solid #ccc;" type="text" name="'.$contact_field['name'].'_rolo_value" value="'.$meta_box_value.'" size="55" tabindex="'.$rolo_tab_index.'" /></p>';
-        $rolo_tab_index++;
-    }
-}
-
-function rolo_create_meta_box() {
-    add_meta_box('rolo_append_meta_box', 'Contact Details', 'new_meta_boxes', 'post', 'normal', 'high');
-}
-
-function rolo_save_postdata( $post_id ) {
-    global $post, $contact_fields;
-    foreach($contact_fields as $contact_field) {
-    // Verify
-        if ( !wp_verify_nonce( $_POST[$contact_field['name'].'_noncename'], plugin_basename(__FILE__) )) {
-            return $post_id;
-        }
-        if ( 'page' == $_POST['post_type'] ) {
-            if ( !current_user_can( 'edit_page', $post_id ))
-                return $post_id;
-        }
-        else {
-            if ( !current_user_can( 'edit_post', $post_id ))
-                return $post_id;
-        }
-
-        $data = $_POST[$contact_field['name'].'_rolo_value'];
-
-        //TODO The meta key name for each contact field should be accessed from the contact_fields array
-        if(get_post_meta($post_id, $contact_field['name'].'_rolo_value') == '')
-            add_post_meta($post_id, $contact_field['name'].'_rolo_value', $data, true);
-        elseif($data != get_post_meta($post_id, $contact_field['name'].'_rolo_value', true))
-            update_post_meta($post_id, $contact_field['name'].'_rolo_value', $data);
-        elseif($data == '')
-            delete_post_meta($post_id, $contact_field['name'].'_rolo_value', get_post_meta($post_id, $contact_field['name'].'_rolo_value', true));
-    }
-}
-
-//add_action('admin_menu', 'rolo_create_meta_box');
-//add_action('save_post', 'rolo_save_postdata');
-
-
-/**
  * Create taxonomies
  * @since 0.1
  */
