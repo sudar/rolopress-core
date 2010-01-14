9b4769a6b4a5c364308e5ee3b95cbf377ae8e3ce
Sudar <sudar@sudarmuthu.com>
2010-01-14 16:48:29 +0000

    Added translation support

--- functions.php
+++ functions.php
@@ -12,6 +12,15 @@
  * @subpackage Functions
  */
 
+// Make theme available for translation
+// Translations can be filed in the /languages/ directory
+load_theme_textdomain( 'rolopress', TEMPLATEPATH . '/languages' );
+ 
+$locale = get_locale();
+$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+if ( is_readable($locale_file) )
+	require_once($locale_file);
+
 // Define constant paths
 define( 'ROLOPRESS_DIR', TEMPLATEPATH );
 define( 'ROLOPRESS_LIBRARY', ROLOPRESS_DIR . '/library' );
