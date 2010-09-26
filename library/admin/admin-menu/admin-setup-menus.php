110158f1423d43467396db88aab934b8f7dfdd59
sbruner <sbruner@slipfire.com>
2010-09-25 23:15:45 -0400

    cleaned up menu system

--- library/admin/admin-menu/admin-setup-menus.php
+++ library/admin/admin-menu/admin-setup-menus.php
@@ -11,58 +11,14 @@
 
 
 function rolo_add_admin_menu() { // adds all top level and sub menus
-  
-  /** Appearance Menu **/
+
+	/** Main RoloPress Menu **/
+	add_menu_page(__('Dashboard','rolopress'), __('rolopress','rolopress'), 'manage_options', 'rolopress-main', 'rolopress_do_page', ROLOPRESS_IMAGES . 'admin/rolopress-icon-bk.gif)');
+		
+	/** Appearance Menu **/
 	add_submenu_page("themes.php", __('Layout Options','rolopress'), __('Layout','rolopress'), 7, 'menu-appearance-layout.php', 'rolo_menu_layout');
-	
-  /** Settings Menu **/
-	add_submenu_page("options-general.php", __('Print Options','rolopress'), __('Printing','rolopress'), 7, 'menu-settings-print.php', 'rolo_print_options');
-	add_submenu_page("options-general.php", __('Search Options','rolopress'), __('Search','rolopress'), 7, 'menu-settings-search.php', 'rolo_search_options');
- 
- /** Contacts Menu **/
-	// Add a new top-level menu:
-	add_menu_page(__('Contacts','rolopress'), __('Contacts','rolopress'), 7, "menu-contacts-settings.php", 'rolo_menu_contacts', ROLOPRESS_IMAGES . '/admin/menu-contact.png');
-
-	// Add sub:
-	add_submenu_page("menu-contacts-settings.php", __('Contact Settings','rolopress'), __('Settings','rolopress'), 7, 'menu-contacts-settings.php', 'rolo_menu_contacts');
-
-	
- /** Companies Menu **/
-	// Add a new top-level menu:
-	add_menu_page(__('Companies','rolopress'), __('Companies','rolopress'), 7, "menu-companies-settings.php", 'rolo_menu_companies', ROLOPRESS_IMAGES . '/admin/menu-company.png');
-
-	// Add sub:
-	add_submenu_page("menu-companies-settings.php", __('Company Settings','rolopress'), __('Settings','rolopress'), 7, 'menu-companies-settings.php', 'rolo_menu_companies');
-	
 
 }
 add_action('admin_menu', 'rolo_add_admin_menu');
-
-
-
-
-
-
-
-
-
-
-
-
-
-/**
- * Setup RoloPress section in admin
- */
-function rolopress_setup_dashboard() {
-	
-		require_once ROLOPRESS_ADMIN_MENU . '/rolopress-admin-settings.php';
-		//require_once ROLOPRESS_ADMIN_MENU . '/rolopress-main-menu.php';
-		add_menu_page(__('Dashboard','rolopress'), __('rolopress','rolopress'), 'manage_options', 'rolopress-main', 'rolopress_do_page', ROLOPRESS_IMAGES . 'admin/rolopress-icon-bk.gif)');
-		
-};
-add_action('admin_menu', 'rolopress_setup_dashboard');
-
-
-
 
 ?>
--- library/admin/admin.php
+++ library/admin/admin.php
@@ -19,21 +19,14 @@ define( 'ROLOPRESS_ADMIN_MENU', ROLOPRESS_ADMIN_FUNCTIONS . '/admin-menu' );
 // Setup overall menu system
 require_once( ROLOPRESS_ADMIN_MENU . '/admin-setup-menus.php' );
 
-// Posts menu
+// Load Main RoloPress menu
+require_once ROLOPRESS_ADMIN_MENU . '/menu-rolopress-main.php';
+
+// Load Posts menu
 require_once( ROLOPRESS_ADMIN_MENU . '/menu-posts-edit.php' );
 
-// Appearance menu
+// Load Appearance menu
 require_once( ROLOPRESS_ADMIN_MENU . '/menu-appearance-layout.php' );
-
-// Settings menu
-require_once( ROLOPRESS_ADMIN_MENU . '/menu-settings-print.php' );
-require_once( ROLOPRESS_ADMIN_MENU . '/menu-settings-search.php' );
-
-// Contacts menu
-require_once( ROLOPRESS_ADMIN_MENU . '/menu-contacts-settings.php' );
-
-// Companies menu
-require_once( ROLOPRESS_ADMIN_MENU . '/menu-companies-settings.php' );
 
 
 // only load if viewing admin
