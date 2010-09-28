6a422061b69cd549a004d571017472201b3d0df7
sbruner <sbruner@slipfire.com>
2010-09-28 14:42:36 -0400

    fixed recent notes widget

--- library/widgets/recent-notes.php
+++ library/widgets/recent-notes.php
@@ -10,51 +10,65 @@
 class Rolo_Widget_Recent_Notes extends WP_Widget {
 
 	function Rolo_Widget_Recent_Notes() {
-		$widget_ops = array( 'classname' => 'recent-notes', 'description' => __('Displays Your Most Recent Notes', 'rolopress') );
-		$control_ops = array( 'width' => 500, 'height' => 350, 'id_base' => 'rolopress-recent-notes' );
-		$this->WP_Widget( 'rolopress-recent-notes', __('Recent Notes', 'rolopress'), $widget_ops, $control_ops );
+		$widget_ops = array('classname' => 'widget_recent_notes', 'description' => __( 'Display your recent Notes' ) );
+		$this->WP_Widget('recent-notes', __('Recent Notes'), $widget_ops);
+		$this->alt_option_name = 'widget_recent_notes';
+
+		if ( is_active_widget(false, false, $this->id_base) )
+			add_action( 'wp_head', array(&$this, 'recent_notes_style') );
 
 		add_action( 'comment_post', array(&$this, 'flush_widget_cache') );
-		add_action( 'wp_set_comment_status', array(&$this, 'flush_widget_cache') );
+		add_action( 'transition_comment_status', array(&$this, 'flush_widget_cache') );
 	}
 
+	function recent_notes_style() { ?>
+	<style type="text/css">.recentnotes a{display:inline !important;padding:0 !important;margin:0 !important;}</style>
+<?php
+	}
 
 	function flush_widget_cache() {
-		wp_cache_delete('recent_notes', 'widget');
+		wp_cache_delete('widget_recent_notes', 'widget');
 	}
 
 	function widget( $args, $instance ) {
-		global $wpdb, $comments, $comment;
+		global $comments, $comment;
 
-		extract($args, EXTR_SKIP);
-		$title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Notes', 'rolopress') : $instance['title']);
-		if ( !$number = (int) $instance['number'] )
-			$number = 5;
-		else if ( $number < 1 )
-			$number = 1;
-		else if ( $number > 15 )
-			$number = 15;
+		$cache = wp_cache_get('widget_recent_notes', 'widget');
 
-		if ( !$comments = wp_cache_get( 'recent_notes', 'widget' ) ) {
-			$comments = $wpdb->get_results("SELECT * FROM $wpdb->comments WHERE comment_approved = '1' ORDER BY comment_date_gmt DESC LIMIT 15");
-			wp_cache_add( 'recent_notes', $comments, 'widget' );
+		if ( ! is_array( $cache ) )
+			$cache = array();
+
+		if ( isset( $cache[$args['widget_id']] ) ) {
+			echo $cache[$args['widget_id']];
+			return;
 		}
 
-		$comments = array_slice( (array) $comments, 0, $number );
-?>
-		<?php echo $before_widget;
-			if ( $title )
-			echo "\n\t\t\t" . $before_title . $title . $after_title;
-			
-			echo "\n\t\t\t\t" . '<ul class="xoxo recent-notes">';
-			
-			if ( $comments ) : foreach ( (array) $comments as $comment) :
-			$output .=  '<li class="recentcomments">' . /* translators: comments widget: 1: comment author, 2: post link */ sprintf(_x('%1$s on %2$s', 'widgets'), get_comment_author_link(), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . '</a>') . '</li>';
-			endforeach; endif;
-			
-			echo "\n\t\t\t\t" . '</ul><!-- .xoxo .recent-notes -->';
-		echo $after_widget; ?>
-<?php
+ 		extract($args, EXTR_SKIP);
+ 		$output = '';
+ 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Recent Notes') : $instance['title']);
+
+		if ( ! $number = (int) $instance['number'] )
+ 			$number = 5;
+ 		else if ( $number < 1 )
+ 			$number = 1;
+
+		$comments = get_comments( array( 'number' => $number, 'status' => 'approve' ) );
+		$output .= $before_widget;
+		if ( $title )
+			$output .= $before_title . $title . $after_title;
+
+		$output .= '<ul id="recentnotes">';
+		if ( $comments ) {
+			foreach ( (array) $comments as $comment) {
+				$output .=  '<li class="recentnotes">' . /* translators: comments widget: 1: comment author, 2: post link */ sprintf(_x('%1$s', 'widgets'), '<a href="' . esc_url( get_comment_link($comment->comment_ID) ) . '">' . get_the_title($comment->comment_post_ID) . ': ' . get_comment_excerpt() . '</a>') . '</li>';
+			}
+ 		}
+		$output .= '</ul>';
+		$output .= $after_widget;
+
+		echo $output;
+		$cache[$args['widget_id']] = $output;
+		wp_cache_set('widget_recent_notes', $cache, 'widget');
 	}
 
 	function update( $new_instance, $old_instance ) {
@@ -71,27 +85,20 @@ class Rolo_Widget_Recent_Notes extends WP_Widget {
 	}
 
 	function form( $instance ) {
-		//Defaults
-		$defaults = array( 'title' => __('Recent Notes', 'rolopress'));
-		$instance = wp_parse_args( (array) $instance, $defaults ); 
-		
 		$title = isset($instance['title']) ? esc_attr($instance['title']) : '';
 		$number = isset($instance['number']) ? absint($instance['number']) : 5;
-		
-
 ?>
 		<div style="float:left;width:98%;">
 		<p><img class="rolo_widget_icon" src= <?php echo ROLOPRESS_IMAGES  . '/admin/rolopress-icon.gif' ?> />
-		<?php _e('Displays your recently created notes.', 'rolopress')?>
+		<?php _e('Displays your recently created Notes.', 'rolopress')?>
 		</p>
 		</div>
-		
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'rolopress'); ?></label>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
 		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" /></p>
 
-		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of notes to show:', 'rolopress'); ?></label>
-		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /><br />
-		<small><?php _e('(at most 15)', 'rolopress'); ?></small></p>
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of notes to show:'); ?></label>
+		<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
 <?php
 	}
-}?>
+}
+?>
