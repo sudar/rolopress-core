5738eaf2f5f99a4552ebd5bf94f1e9c36681e7bf
sbruner <sbruner@slipfire.com>
2010-10-04 11:01:04 -0400

    simple comment change

--- library/admin/admin-menu/admin-setup-menus.php
+++ library/admin/admin-menu/admin-setup-menus.php
@@ -20,8 +20,9 @@ function rolo_add_admin_menu() { // adds all top level and sub menus
 	
 	/** Contacts Menu **/
 	add_submenu_page("rolopress-main", __('Contact Settings','rolopress'), __('Contacts','rolopress'), 7, 'menu-rolopress-contact.php', 'rolopress_do_contact_settings_page');
+	
+	/** Company Menu **/
 	add_submenu_page("rolopress-main", __('Company Settings','rolopress'), __('Companies','rolopress'), 7, 'menu-rolopress-company.php', 'rolopress_do_company_settings_page');
-
 
 }
 add_action('admin_menu', 'rolo_add_admin_menu');
