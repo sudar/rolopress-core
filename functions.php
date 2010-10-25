6073ef456f34cfee2b55379077acd22173c0d3eb
sbruner <sbruner@slipfire.com>
2010-10-25 14:46:34 -0400

    define rolopress_version

--- functions.php
+++ functions.php
@@ -21,10 +21,8 @@ $locale_file = TEMPLATEPATH . "/languages/$locale.php";
 if ( is_readable($locale_file) )
 	require_once($locale_file);
 	
-define ( 'ROLOPRESS_CORE_THEME_VERSION', '1.4');
-
-//To check if RoloPress is loaded or not.
-define('ROLOPRESS_VERSION', '1.4');
+// Define theme version
+define ( 'ROLOPRESS_VERSION', '1.5');
 
 // Define constant paths
 define( 'ROLOPRESS_DIR', TEMPLATEPATH );
